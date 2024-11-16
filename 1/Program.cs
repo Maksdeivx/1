02b154e2f7f9b6f3f80345bcd650242a601edf68
Maks_Kozeyko <maks.kozeyko@gmail.com>
2024-11-16 17:13:56 +0300

    поменяд знак

--- 1/Program.cs
+++ 1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static double Func(double x1, double y1, double x2, double y2)
     {
-        double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        double d = Math.Sqrt(Math.Pow(x2 + x1, 2) + Math.Pow(y2 - y1, 2));
         return d;
     }
     static double min(double a, double b)
